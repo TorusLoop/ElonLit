cc48549826b3826e3d56bef378dca9235ec90a25
init-infinity <99610706+init-infinity@users.noreply.github.com>
2022-04-10 16:00:29 +0100

    Add files via upload

--- contracts/token.sol
+++ contracts/token.sol
@@ -5,25 +5,36 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
-contract ElonLit is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, ERC20Permit, ERC20Votes, ERC20FlashMint {
-    constructor() ERC20("ElonLit", "ELIT") ERC20Permit("ElonLit") {}
+contract FooBar is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable, ERC20Permit, ERC20Votes {
+    constructor() ERC20("FooBar", "FOOBAR") ERC20Permit("FooBar") {
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
 
     function snapshot() public onlyOwner {
         _snapshot();
     }
 
-    // The following functions are overrides required by Solidity.
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
+        whenNotPaused
         override(ERC20, ERC20Snapshot)
     {
         super._beforeTokenTransfer(from, to, amount);
     }
+
+    // The following functions are overrides required by Solidity.
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
         internal
